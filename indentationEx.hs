29168f96ed24d266c7433977c8d6f0d0c6f6c807
111202214613@mhs.dinusnac.id <111202214613@mhs.dinus.ac.id>
2023-12-02 11:27:24 +0700

    First Commit Belajar Haskel Untuk Pemula

---
+++ indentationEx.hs
@@ -0,0 +1,31 @@
+-- main :: IO ()
+-- main = do
+--     let 
+--      x = 10
+--      y = 5
+    
+--     print(x,y)
+
+-- -- tambah2 x = x + 2
+
+-- tambah2 x = 
+--     x + 2
+-- tambah2 x = 
+-- x + 2
+-- main :: IO ()
+-- main = do putStrLn "What is 2 + 2?"
+--           x <- readLn
+--           if x == 4
+--               then putStrLn "You're right!"
+--               else putStrLn "You're wrong!"
+
+-- main = do {
+-- putStrLn "What is 2 + 2?";
+-- x <- readLn;
+-- if x == 4 
+-- then putStrLn "You're right!" 
+-- else putStrLn "You're wrong!"
+-- } 
+
+-- dalam 1 baris
+main :: IO (); main = do { putStrLn "What is 2 + 2?"; x <- readLn; if x == 4; then putStrLn "You're right!"; else putStrLn "You're wrong!";}
---
+++ myProram.hs
@@ -0,0 +1,8 @@
+main :: IO () -- mendeklarasikan tipe data dari main
+
+{-
+  Kode di bawah ini akan mencetak
+  teks "Hello, World!" ke layar.
+-}
+
+main = putStrLn "Hello, World!"
